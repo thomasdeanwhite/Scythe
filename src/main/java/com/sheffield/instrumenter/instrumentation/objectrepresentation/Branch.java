faf9b31a7ec1ec5b3c6b9b2481d7841469f14783
David Paterson <dpaterson1@sheffield.ac.uk>
2016-05-05 17:23:19 +0100

    lots of changes

--- src/main/java/com/sheffield/instrumenter/instrumentation/objectrepresentation/Branch.java
+++ src/main/java/com/sheffield/instrumenter/instrumentation/objectrepresentation/Branch.java
@@ -1,51 +1,67 @@
 package com.sheffield.instrumenter.instrumentation.objectrepresentation;
 
 public class Branch extends CoverableGoal {
-	private int trueHits;
-	private int falseHits;
+  private int trueHits;
+  private int falseHits;
 
-	public Branch(String className, int lineNumber) {
-		super(className, lineNumber);
-	}
+  public Branch(String className, int lineNumber) {
+    super(className, lineNumber);
+  }
 
-	public void trueHit(int trueHits) {
-		this.trueHits += trueHits;
-	}
+  public void trueHit(int trueHits) {
+    this.trueHits += trueHits;
+  }
 
-	public void falseHit(int falseHits) {
-		this.falseHits += falseHits;
-	}
+  public void falseHit(int falseHits) {
+    this.falseHits += falseHits;
+  }
 
-	public int getTrueHits() {
-		return trueHits;
-	}
+  public int getTrueHits() {
+    return trueHits;
+  }
 
-	public int getFalseHits() {
-		return falseHits;
-	}
+  public int getFalseHits() {
+    return falseHits;
+  }
 
-	public void reset() {
-		trueHits = 0;
-		falseHits = 0;
-	}
+  @Override
+  public void reset() {
+    trueHits = 0;
+    falseHits = 0;
+  }
 
-	@Override
-	public String toString() {
-		return lineNumber + "#true, " + lineNumber + "#false";
-	}
+  @Override
+  public String toString() {
+    return lineNumber + "#true, " + lineNumber + "#false";
+  }
 
-	public boolean equals(Branch other) {
-		if (this == other) {
-			return true;
-		}
-		if (lineNumber == other.lineNumber && className.equals(other.className)) {
-			return true;
-		}
-		return false;
-	}
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) {
+      return true;
+    }
+    if (!(other instanceof Branch)) {
+      return false;
+    }
+    if (lineNumber == ((Branch) other).lineNumber && className.equals(((Branch) other).className)
+        && goalId == ((Branch) other).goalId) {
+      return true;
+    }
+    return false;
+  }
 
-	public void setClassName (String name){
-		className = name;
-	}
+  @Override
+  public void setClassName(String name) {
+    className = name;
+  }
+
+  @Override
+  public Branch clone() {
+    Branch clone = new Branch(className, lineNumber);
+    clone.setGoalId(goalId);
+    clone.trueHits = trueHits;
+    clone.falseHits = falseHits;
+    return clone;
+  }
 
 }
--- src/main/java/com/sheffield/util/ArrayUtils.java
+++ src/main/java/com/sheffield/util/ArrayUtils.java
@@ -1,38 +1,49 @@
 package com.sheffield.util;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * Created by david on 07/04/16.
  */
 public class ArrayUtils {
-    public static <T> T[] combineArrays(Class<T> type, T[] first, T[] second) {
-        return combineArrays(type, false, first, second);
-    }
+  public static <T> T[] combineArrays(Class<T> type, T[] first, T[] second) {
+    return combineArrays(type, false, first, second);
+  }
 
-    public static <T> T[] combineArrays(Class<T> type, boolean unique, T[]... arrays) {
-        int size = 0;
-        for (T[] arr : arrays){
-            size += arr.length;
-        }
-        T[] result = (T[]) Array.newInstance(type, size);
-        int current = 0;
-        for(int i = 0; i < arrays.length; i++){
-            T[] arr = arrays[i];
-            for(T el : arr){
-                result[current] = el;
-                current++;
-            }
-        }
-        if (unique) {
-
-            Set<T> set = new HashSet<T>();
-            set.addAll(Arrays.asList(result));
-            result = set.toArray((T[]) Array.newInstance(type, set.size()));
-        }
-        return (T[]) result;
+  public static <T> T[] combineArrays(Class<T> type, boolean unique, T[]... arrays) {
+    int size = 0;
+    for (T[] arr : arrays) {
+      size += arr.length;
     }
+    T[] result = (T[]) Array.newInstance(type, size);
+    int current = 0;
+    for (int i = 0; i < arrays.length; i++) {
+      T[] arr = arrays[i];
+      for (T el : arr) {
+        result[current] = el;
+        current++;
+      }
+    }
+    if (unique) {
+
+      Set<T> set = new HashSet<T>();
+      set.addAll(Arrays.asList(result));
+      result = set.toArray((T[]) Array.newInstance(type, set.size()));
+    }
+    return result;
+  }
+
+  public static <T> List<T> createList(T... objects) {
+    List<T> list = new ArrayList<T>();
+    for (T object : objects) {
+      list.add(object);
+    }
+    return list;
+  }
+
 }
