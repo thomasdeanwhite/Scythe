89913e5f2ca6a36769793a73b280dba9c59ba1d5
David Paterson <dpaterson1@sheffield.ac.uk>
2016-11-08 17:26:08 +0000

    fix for branch coverage

--- src/main/java/com/sheffield/instrumenter/analysis/ClassAnalyzer.java
+++ src/main/java/com/sheffield/instrumenter/analysis/ClassAnalyzer.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -684,7 +685,8 @@ public class ClassAnalyzer {
       return Collections.<Line> emptyList();
     }
     List<Line> coverableLines = new ArrayList<Line>();
-    for (LineHit lh : lines.get(classId).values()) {
+    Collection<LineHit> lhs = lines.get(classId).values();
+    for (LineHit lh : lhs) {
       coverableLines.add(lh.getLine());
     }
     return coverableLines;
